a479049c278b91be4f7b9dd253bcb36644def4e9
tsundev <tsundev@gmail.com>
2016-03-30 16:18:00 +0100

    Create Test
    Test php file created.

---
+++ test.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en-GB">
+
+<head>
+	<title>Test</title>
+	<meta name="author" content="TsunDev">
+	<meta charset="UTF-8">
+</head>
+
+<body>
+<div class="row">
+
+<?php
+  echo 'hello';
+?>
+
+</div>
+</body>
+</html>
